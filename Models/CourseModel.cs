d0bcae95e5743f6073efe1419b7419cf371f8e84
Pruthvi <pruthvisoni0505@gmail.com>
2023-07-30 20:25:22 -0400

    Made Models

---
+++ Models/CourseModel.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace SQLiteDemo.Models
+{
+    public class CourseModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int CourseID { get; set; }
+
+        public string CourseName { get; set; }
+    }
+}
---
+++ Models/StudentModel.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+
+namespace SQLiteDemo.Models
+{
+    public class StudentModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int StudentID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        [Ignore]
+        public string FullName => $"{FirstName} {LastName}";
+
+        // Property to represent the course assignment
+        [Indexed] // Add this attribute to create an index for faster lookup
+        public int AssignedCourseID { get; set; }
+    }
+}
